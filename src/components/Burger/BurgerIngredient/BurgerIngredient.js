ca21931e1acacc30dcb2dd47076cd923b2ca0dae
Magno Sescon <magnosescon@gmail.com>
2020-07-25 11:56:49 -0300

    created the Burger component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import classes from './BurgerIngredients.module.css';
+import classes from './BurgerIngredient.module.css';
 
 const burgerIngredient = (props) => {
 	let ingredient = null;
