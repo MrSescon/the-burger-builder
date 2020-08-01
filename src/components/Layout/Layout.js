719620d0088f3a53a659d458fc395e08bc58c7ac
Magno Sescon <magnosescon@gmail.com>
2020-08-01 09:53:38 -0300

    created a responsive sideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,9 +3,11 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
 import ToolBar from '../Navigation/ToolBar/ToolBar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
 	<Aux>
+		<SideDrawer />
 		<ToolBar />
 		<main className={classes.Content}>{props.children}</main>
 	</Aux>
