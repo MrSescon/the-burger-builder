e2de1d30b22df8ae5a9a8ec26a2b41093cd717c9
Magno Sescon <magnosescon@gmail.com>
2020-08-01 09:37:49 -0300

    Added Reusable Navigation

--- src/components/Navigation/ToolBar/ToolBar.js
+++ src/components/Navigation/ToolBar/ToolBar.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import classes from './ToolBar.module.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolBar = (props) => (
 	<header className={classes.ToolBar}>
 		<div>MENU</div>
 		<Logo />
-		<nav>...</nav>
+		<nav>
+			<NavigationItems />
+		</nav>
 	</header>
 );
 
