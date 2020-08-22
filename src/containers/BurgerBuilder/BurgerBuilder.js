1aee7550aa5f94ec9a09f327730f24232a619085
Magno Sescon <magnosescon@gmail.com>
2020-08-22 11:58:42 -0300

    Created a navigation into the Contact Data component and implemented the Order submission / Passing Data Between pages

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -46,34 +46,13 @@ class BurgerBuilder extends Component {
 
 	purchaseContinueHandler = () => {
 		//alert('you continue');
-		// this.setState({ loading: true });
-		// const order = {
-		// 	ingredients: this.state.ingredients,
-		// 	price: this.state.totalPrice,
-		// 	costumer: {
-		// 		name: 'Magno Sescon',
-		// 		address: {
-		// 			street: 'Teststreet 1',
-		// 			zipcode: '99999',
-		// 			country: 'Germany'
-		// 		},
-		// 		email: 'teste@test.com'
-		// 	},
-		// 	deliveryMethod: 'fastest'
-		// };
-		// axios
-		// 	.post('/orders.json', order)
-		// 	.then((response) => {
-		// 		this.setState({ loading: false, purchasing: false });
-		// 	})
-		// 	.catch((error) => {
-		// 		this.setState({ loading: false, purchasing: false });
-		// 	});
+
 		const queryParams = [];
 		for (let i in this.state.ingredients) {
 			queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
 		}
 		const querString = queryParams.join('&');
+		queryParams.push('price=' + this.state.totalPrice);
 		this.props.history.push({
 			pathname: '/checkout',
 			search: '?' + querString
